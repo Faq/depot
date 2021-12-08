150f5f161a2e9b6d2044582b1cdf8265cddf0ae7
FAQ <ainarsh@gmail.com>
2021-12-08 18:15:22 +0200

    Revert "Playtime: bin/rails generate migration add_price_to_line_items price:decimal"
    This reverts commit dfed10b9b65f7bc8229a57328ebae9c4749b0d7a.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,16 +3,12 @@ class Cart < ApplicationRecord
 
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
-
     if current_item
       current_item.quantity += 1
-      current_item.price = current_item.quantity * product.price
     else
       current_item = line_items.build(product_id: product.id)
-      current_item.price = product.price
     end
     current_item
-
   end
 
   def total_price
