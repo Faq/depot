af4b764eb1441c8c7274baeec0673f37da821013
FAQ <ainarsh@gmail.com>
2021-12-14 17:24:01 +0200

    setting up pago and use bin/rails generate job charge_order

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,16 +73,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  #
-  Rails.application.configure do config.action_mailer.delivery_method = :test
-  #config.action_mailer.smtp_settings = {
-  #  address: "smtp.gmail.com",
-  #  port: 587,
-  #  domain: "example.com",
-  #  authentication: "plain",
-  #  enable_starttls_auto: true,
-  #  user_name: ENV["GMAIL_USERNAME"],
-  #  password: ENV["GMAIL_PASSWORD"]
-  #}
-  end
 end
